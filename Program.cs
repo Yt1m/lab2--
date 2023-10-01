19377274c8426eb525d369927bf904f08372bce6
Yt1m <ytimrdonqaz@gmail.com>
2023-10-01 16:42:42 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Введите m:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите n:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите k:");
+int k = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите l:");
+int l = int.Parse(Console.ReadLine());
+Console.WriteLine(((n + m) > k) ^ (n > k && m < l));
